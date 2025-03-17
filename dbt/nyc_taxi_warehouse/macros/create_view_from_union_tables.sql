852a24db27b921434d0014133f155885555c094d
yokin Zou <yokinzou@yokins-MacBook-Air.local>
2025-03-17 23:30:17 +0800

    update dbt

--- dbt/nyc_taxi_warehouse/macros/create_view_from_union_tables.sql
+++
@@ -1,4 +0,0 @@
-{% macro create_view_from_union_tables(view_name, table_name_pattern) %}
-    create or replace view {{ target.schema }}.{{ view_name }} as
-    {{ union_all_tables('raw', table_name_pattern) }} 
-{% endmacro %}          
